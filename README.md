7e8ab8fdbf535815e607721060efcca7d459953c
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-01-23 17:58:35 -0800

    ReadMe updated. Textareas fixed to span whole width and height, save column changed to buttons

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Daily-Planner
+
+A daily workday calendar set in military time. Use the center box to add your hourly assignments and click the save button on the right!
+Each hour is color-coded depending on if you are past, in, or ahead of that hour.
+
+## Live Site
+
+
+---
+Erinn Matesi 2021-2022 UW Bootcamp
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 var saveBtn = document.querySelector("saveBtn");
-var hour = document.querySelector("hour");
+var $hour = $("hour");
 
-var date = moment().format("dddd, MMMM Do YYYY");
+var date = moment().format("dddd, MMMM Do YYYY, HH:mm");
 $("#currentDay").text(date);
+var currentTime = moment().format("HH");
 
-// this needs to become jquery so I can use addClass
-if (date > hour) {
-    $("hour").addClass("past")
-} else if (date < hour) {
-    $("hour").addClass("future")
+// this is not working as intended
+if (currentTime > $hour) {
+    $hour.addClass("past")
+} else if (date < $hour) {
+    $hour.addClass("future")
 } else {
-    $("hour").addClass("present")
-}
+    $hour.addClass("present")
+};
 
 
 
@@ -21,10 +22,13 @@ var save;
 
 $("saveBtn").click(function(event) {
     event.preventDefault();
-    // localStorage.setItem ("task", "???")
+    // localStorage need to grab the textarea content but keep it assigned to it's appropriate row - does it need to be attached to a specific hour? Does each hour column need an id to go with the matching text area?
+    // localStorage.setItem ("task", "textarea.value")
 });
-// which should I use?
+// should I use vanilla js??
 // saveBtn.addEventListener("click", function(event) {
 //     event.preventDefault();
 //     // localStorage.setItem ("task", "???")
 // });
+
+// How do I then localStorage.getItem and place it in the appropriate row and column?
